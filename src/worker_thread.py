c1e47c7c93799f5ec2dc308670b8a4e4a8335340
argon <kouta528bado@yahoo.co.jp>
2019-04-24 17:54:19 +0900

    modify a process of socket.recv

--- src/worker_thread.py
+++ src/worker_thread.py
@@ -21,18 +21,18 @@ class WorkerThread(threading.Thread):
 
         while byte_msg:
             sum_msg += byte_msg.decode('utf-8')
+            print("request:", sum_msg)
+            print("request length:", len(sum_msg))
+
+            response: Response = self.handle_request(sum_msg)
+
+            if response:
+                send_response.send_response(self.sock,response)
+            else:
+                pass
+
             byte_msg = self.sock.recv(1024)
 
-        print("request:", sum_msg)
-        print("request length:", len(sum_msg))
-
-        response: Response = self.handle_request(sum_msg)
-
-        if response:
-            send_response.send_response(self.sock,response)
-        else:
-            pass
-        
         self.sock.close()
 
 
